25c7e2368d430eeeea0012c82a8037de77d742e5
mark-weassist-io <mark@weassist.io>
2026-05-12 19:58:27 +0800

    auto: ops.tsx [19:58:27]

--- src/routes/ops.tsx
+++ src/routes/ops.tsx
@@ -2,6 +2,7 @@ import { Hono } from "hono"
 import { z } from "zod"
 import { OpsListPage } from "../views/pages/OpsList"
 import { getOpsWithAssignments, getOpById, getOpsCount, createOp, updateOp, softDeleteOp, restoreOp, getOpPhones } from "../db/queries/ops"
+import { getDb } from "../db"
 import { getCsStaff } from "../db/queries/cs-staff"
 
 const router = new Hono()
