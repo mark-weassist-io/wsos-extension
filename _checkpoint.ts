9d8e116eec23fcf3b04c7aee037c82c06e58ccc5
mark-weassist-io <mark@weassist.io>
2026-05-13 11:13:11 +0800

    Clean up temp deployment scripts

--- _checkpoint.ts
+++
@@ -1,6 +0,0 @@
-import { Database } from "bun:sqlite"
-const db = new Database("data/wsos-extension.db")
-db.run("PRAGMA wal_checkpoint(TRUNCATE)")
-db.run("PRAGMA journal_mode=DELETE")
-db.close()
-console.log("ok")
